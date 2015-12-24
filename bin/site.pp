bab67c375ce5dade5bd44d41169d44418b36e59e
codylane <cody.lane@gmail.com>
2015-12-23 19:20:28 -0700

    fixing a few minor bugs to show just an example

--- bin/site.pp
+++ bin/site.pp
@@ -1,11 +1,18 @@
 define create_nodeset(
+  $project_dir,
   $project_name    = $title,
   $beaker_nodesets = hiera('beaker_nodesets'),
 ) {
+
+  class { 'beaker_init':
+    project_dir => $project_dir,
+  }
 
    create_resources(beaker_init::nodeset, $beaker_nodesets[$project_name])
 }
 
 node default {
-  create_nodeset { 'beaker_init': }
+  create_nodeset { 'example_project':
+    project_dir => '/tmp/example_project'
+  }
 }
