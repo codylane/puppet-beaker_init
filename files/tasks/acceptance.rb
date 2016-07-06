ee0278bcb0e79729b4ae529e37653f80831cf467
codylane <cody.lane@gmail.com>
2016-07-06 11:23:58 -0600

    adding updated environment variables for rake tasks

--- files/tasks/acceptance.rb
+++ files/tasks/acceptance.rb
@@ -4,10 +4,17 @@ desc "Acceptance tests"
 namespace :acceptance do
 
   task :tests do
-     set_provisioned_env(has_been_provisioned)
-     Rake::Task[:beaker].invoke
 
-     File.write($PROVISIONED_FILENAME, '')
+    if ENV['PUPPET_INSTALL_VERSION'].nil?
+      ENV['PUPPET_INSTALL_VERSION'] = '1.4.1'
+      ENV['PUPPET_INSTALL_TYPE'] = 'agent'
+      ENV['BEAKER_provision'] = 'yes'
+    end
+
+    set_provisioned_env(has_been_provisioned)
+    Rake::Task[:beaker].invoke
+
+    File.write($PROVISIONED_FILENAME, '')
   end
 
 end
--- files/tasks/create.rb
+++ files/tasks/create.rb
@@ -2,11 +2,16 @@ require_relative "helpers"
 
 desc "Starts up the VM, provisions and keeps it running so you can run more tests"
 task :create do
-    ENV['BEAKER_destroy'] = 'no'
+  ENV['BEAKER_destroy'] = 'no'
 
-   Rake::Task[:validate].invoke
-   Rake::Task[:spec].invoke
-   Rake::Task[:beaker].invoke
+  if ENV['PUPPET_INSTALL_VERSION'].nil?
+    ENV['PUPPET_INSTALL_VERSION'] = '1.4.1'
+    ENV['PUPPET_INSTALL_TYPE'] = 'agent'
+  end
 
-   File.write($PROVISIONED_FILENAME, '')
+  Rake::Task[:validate].invoke
+  Rake::Task[:spec].invoke
+  Rake::Task[:beaker].invoke
+
+  File.write($PROVISIONED_FILENAME, '')
 end
